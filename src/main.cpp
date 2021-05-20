52646d7875c585b97170ae186dd9e2a3559bbc40
Dmitrii Potapov <potapov627@yandex.ru>
2021-05-20 16:07:18 +0300

    Optimizing code

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,18 @@
 #define MOTOR 5
 #define ENABLE 6
 #define PRESSURE_SENSOR A0
-#define N 60
-#define START 20
+#define N 60     // Разрешение энкодера
+#define START 20 // Номер оборота начала фиксации данных
 
 bool start_flag = false;
 bool send_flag = false;
-volatile int num_revolutions = 0;
-volatile int count = 0; // Счетчик оборотов
-volatile int state = 0; // Переменная хранящая статус вращения
-
-volatile int pinAValue = 0; // Переменные хранящие состояние пина
+volatile int num_revolutions = 0; // Счетчик оборотов
+volatile int count = 0;           // Счетчик импульсов оборота
+volatile int state = 0;           // Переменная хранящая статус вращения
+volatile int pinAValue = 0;       // Переменные хранящие состояние пина импульсов энкодера
 volatile int pinBValue = 0;
 int **pressure_data; // Данные эксперимента
-byte num_rep;        // Кроличество итераций эксперимента
+byte num_rep;        // Количество итераций эксперимента
 
 void establishContact()
 {
@@ -30,7 +29,7 @@ void establishContact()
     delay(150);
     digitalWrite(LED_BUILTIN, LOW);
     delay(150);
-    response = Serial.read();
+    response = Serial.read(); // Обработка подтверждения соединения
     if (response == 'A')
     {
       return;
@@ -38,12 +37,11 @@ void establishContact()
   }
 }
 
-void A()
+void A() // Прерывание по фазе A
 {
   int pressure;
   pinAValue = digitalRead(ENCODER_A); // Получаем состояние пинов A и B
   pinBValue = digitalRead(ENCODER_B);
-
   cli(); // Запрещаем обработку прерываний
   if (!pinAValue && pinBValue)
     state = 1; // Если при спаде линии А на линии B лог. единица, то вращение в одну сторону
@@ -57,7 +55,7 @@ void A()
       state = 0;
     }
   }
-  if (start_flag == true && count < 60) // Запись данных
+  if (start_flag == true && count < 60) // Чтение и сохранение данных давления
   {
     pressure = analogRead(PRESSURE_SENSOR);
     pressure_data[num_revolutions - START][count] = pressure;
@@ -65,29 +63,29 @@ void A()
   sei(); // Разрешаем обработку прерываний
 }
 
-void Z()
+void Z() // Прерывание по фазе Z
 {
   int pressure;
   count = 0;
   num_revolutions++;
   if (num_revolutions == START)
     start_flag = true;
-  if (start_flag == true)
+  if (start_flag == true) // Чтение и сохранение давления в нулевой точке текущего оборота
   {
     pressure = analogRead(PRESSURE_SENSOR);
     pressure_data[num_revolutions - START][count] = pressure;
   }
 
-  if (num_revolutions == START + num_rep)
+  if (num_revolutions == START + num_rep) // Окончание эксперимента
   {
     start_flag = false;
-    analogWrite(MOTOR, 0);
     digitalWrite(ENABLE, LOW);
+    analogWrite(MOTOR, 0);
     send_flag = true;
   }
 }
 
-void init_data_array()
+void init_data_array() // Инициализация массива данных
 {
   for (int i = 0; i < num_rep; i++)
   {
@@ -98,7 +96,7 @@ void init_data_array()
   }
 }
 
-void send_data()
+void send_data() // Отправка данных эксперимента
 {
   char *ptr;
   for (int i = 0; i < num_rep; i++)
@@ -110,6 +108,10 @@ void send_data()
       Serial.write(*ptr++); //Отправляем старший байт
     }
   }
+}
+
+void delete_data()
+{ // Очищение массива данных
   for (int i = 0; i < num_rep; i++)
     delete pressure_data[i];
   delete[] pressure_data;
@@ -119,7 +121,7 @@ void setup()
 {
   pinMode(PRESSURE_SENSOR, INPUT);
   ADCSRA |= (1 << ADPS2); //Биту ADPS2 присваиваем единицу - коэффициент деления 16
-  ADCSRA &= ~((1 << ADPS1) | (1 << ADPS0));
+  ADCSRA &= ~((1 << ADPS1) | (1 << ADPS0)); //Битам ADPS1 и ADPS0 присваиваем нули
   pinMode(ENCODER_A, INPUT);
   pinMode(ENCODER_B, INPUT);
   pinMode(ENCODER_Z, INPUT);
@@ -127,8 +129,8 @@ void setup()
   pinMode(ENABLE, OUTPUT);
   digitalWrite(ENABLE, LOW);
   pinMode(LED_BUILTIN, OUTPUT);
-  attachInterrupt(1, Z, RISING);
-  attachInterrupt(0, A, CHANGE);
+  attachInterrupt(1, Z, RISING); // Настройка прерывания индексного сигнала энкодера
+  attachInterrupt(0, A, CHANGE); // Настройка прерывания сигнала A энкодера
   Serial.begin(9600);
   while (!Serial)
   {
@@ -163,6 +165,9 @@ void loop()
   if (send_flag == true)
   {
     send_data();
+    delete_data();
+    num_revolutions = 0;
+    count = 0;
     send_flag = false;
   }
 }
